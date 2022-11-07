6cd41f071183c0033125475ddbc1ace553203e4b
Jut257 <117411394+Jut257@users.noreply.github.com>
2022-11-07 00:14:06 -0500

    Add files via upload
    the phase 2 and 3 clean file essentially just hold the visualization parts

--- Phase_2_and_3_clean.py
+++ Phase_2_and_3_clean.py
@@ -45,52 +45,58 @@ X = scaler.transform(X)
 dfTrain2Numerical.is_copy = False
 dfTrain2Numerical[['NumNumericChars_Standardized','NumDots_Standardized','SubdomainLevel_Standardized','PathLevel_Standardized','UrlLength_Standardized','NumDash_Standardized','NumDashInHostname_Standardized','NumUnderscore_Standardized','NumPercent_Standardized','NumQueryComponents_Standardized','NumAmpersand_Standardized','NumHash_Standardized','HostnameLength_Standardized','PathLength_Standardized','QueryLength_Standardized','NumSensitiveWords_Standardized']]=X
 ################################# Phase 2 and 3 ###################################################################
-dfTrainStandardized = dfTrain2Numerical
-
-### Identify the features that are highly correlated ###
-## Manhattan distance ##
-# euclidean isnt good for multiple features
+#dfTrainStandardized = dfTrain2Numerical
+dfTrain2Numerical= dfTrain2Numerical[['UrlLength_Standardized','NumNumericChars_Standardized','NumDots_Standardized','SubdomainLevel_Standardized','PathLevel_Standardized','NumDash_Standardized','NumDashInHostname_Standardized','NumUnderscore_Standardized','NumPercent_Standardized','NumQueryComponents_Standardized','NumAmpersand_Standardized','NumHash_Standardized','HostnameLength_Standardized','PathLength_Standardized','QueryLength_Standardized','NumSensitiveWords_Standardized']]
+# Next, must combine dfTrain2Numerical with binary data still in dfTrain2
+dfTrain3= dfTrain2Numerical.join(dfTrain2[['AtSymbol','TildeSymbol','NoHttps','RandomString','IpAddress','DomainInSubdomains','DomainInPaths','HttpsInHostname','DoubleSlashInPath','EmbeddedBrandName','PctExtResourceUrls','ExtFavicon','InsecureForms','RelativeFormAction','ExtFormAction','RightClickDisabled','PopUpWindow','IframeOrFrame','MissingTitle','ImagesOnlyInForm']])
+# Should drop 'HttpsInHostname' as each case =0. Will mess up correlation otherwise
+dfTrain3=dfTrain3.drop('HttpsInHostname',axis=1)
+# Perform standard feature correlation on entirety of dfTrain3 by feature
+dfTrain3corr=dfTrain3.corr()
+class_labels = dfTrain2['CLASS_LABEL']
+# Perform manhattan measure
 from sklearn.metrics.pairwise import manhattan_distances
-distallpairs = manhattan_distances(dfTrainStandardized.iloc[0:30],dfTrainStandardized[0:30])
-print(distallpairs)
-# all cases against all cases
-#dist_test = manhattan_distances(dfTrainStandardized)
+distallpairs = manhattan_distances(dfTrain3.iloc[0:35],dfTrain3[0:35])
+print("Manhattan distance among all cases:\n", distallpairs)
 
-######### needs work/ might not be viable ##########
+### Evaluating Cluster Quality ###
 from sklearn.metrics import silhouette_samples
-labels=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
-silh = silhouette_samples(dfTrainStandardized, labels)
-################################
+silh = silhouette_samples(dfTrain3,class_labels)
+print(silh)
+
+plt.figure()
+plt.bar(range(0,35),silh,width=0.8)
+plt.xticks(range(0,35))
+plt.show()
 
 #####################
 ### cluster model ###
 from sklearn.cluster import KMeans
 ###############################################################
-cluster_model = KMeans(n_clusters=30, init="k-means++",max_iter=300, n_init=15, random_state=0)
-cluster_model.fit(dfTrainStandardized)
+cluster_model = KMeans(n_clusters=35, init="k-means++",max_iter=300, n_init=15, random_state=0)
+cluster_model.fit(dfTrain3)
 centers = cluster_model.cluster_centers_
 print(centers)
 inertia = cluster_model.inertia_
 print(inertia)
 plt.figure()
-pred_y = cluster_model.fit_predict(dfTrainStandardized)
-# testing different vars
-plt.scatter(dfTrainStandardized, dfTrainStandardized.iloc[1:30], c = pred_y.astype(float), s=10)
+pred_y = cluster_model.fit_predict(dfTrain3)
+ 
+plt.scatter(dfTrain3.iloc[0:34], dfTrain3, c = pred_y.astype(float), s=10)
 plt.scatter(centers[:,0], centers[:,1], c='blue', s=50)
 plt.show()
-
-print(list(dfTrainStandardized[0:30].columns))
 ###############################################################
+
 ## elbow method ##
 inertia_elbow = []
-for k in range (1,30):
+for k in range (1,35):
     cluster_model_elbow = KMeans(n_clusters=k, init="k-means++",max_iter=300, n_init=10, random_state=0)
-    cluster_model_elbow.fit(dfTrainStandardized)
+    cluster_model_elbow.fit(dfTrain3)
     inertia_elbow.append(cluster_model_elbow.inertia_)
     
 plt.figure()
 print(inertia_elbow)
-plt.plot(range(1,30),inertia_elbow)
+plt.plot(range(1,35),inertia_elbow)
 plt.xlabel('K')
 plt.ylabel('inertia') 
 plt.show()   
@@ -99,14 +105,14 @@ plt.show()
 from sklearn.metrics import silhouette_score
 
 sil=[]
-for k in range (2,30):
+for k in range (2,34):
     cluster_model_sil = KMeans(n_clusters = k, init='k-means++', max_iter=300, n_init=10, random_state=0)
-    pred_y = cluster_model_sil.fit_predict(dfTrainStandardized)
-    sil.append(silhouette_score(dfTrainStandardized, pred_y, metric = 'euclidean'))
+    pred_y = cluster_model_sil.fit_predict(dfTrain3)
+    sil.append(silhouette_score(dfTrain3, pred_y, metric = 'euclidean'))
 
 plt.figure()
 print(sil)
-plt.plot(range(2,30),sil)
+plt.plot(range(2,34),sil)
 plt.title('silhouette method')
 plt.xlabel('k')
 plt.ylabel('silhouette')
@@ -120,7 +126,7 @@ for i in [2,3,4,5,6,7,8,9,10,11,12,13,14,15]:
     cluster_model_YB = KMeans(n_clusters = i, init='k-means++', max_iter=100, n_init=10, random_state=0)
     q, mod = divmod(i,2)
     visualizer = SilhouetteVisualizer(cluster_model_YB, colors='yellowbrick', ax=ax[q-1][mod])
-    visualizer.fit(dfTrainStandardized)
+    visualizer.fit(dfTrain3)
   
 ### Hierarchical Clustering ###
 ## Dendrogram ##
@@ -129,8 +135,8 @@ for i in [2,3,4,5,6,7,8,9,10,11,12,13,14,15]:
 # find out names of cases to narrow down the 7
 import scipy.cluster.hierarchy as sch
 plt.figure()
-dend = sch.dendrogram(sch.linkage(dfTrainStandardized, method = 'ward'))
-plt.axhline(y=750, linestyle='--', c='orange')
+dend = sch.dendrogram(sch.linkage(dfTrain3, method = 'ward'))
+plt.axhline(y=75, linestyle='--', c='orange')
 plt.title('dendrogram')
 plt.xlabel('cases')
 plt.ylabel('distance')
@@ -139,10 +145,10 @@ plt.show()
 ## Agglomerative Clustering ##
 from sklearn.cluster import AgglomerativeClustering
 cluster_model_Agglo = AgglomerativeClustering(n_clusters=3, affinity='euclidean', linkage='ward')
-cluster_model_Agglo.fit(dfTrainStandardized)
+cluster_model_Agglo.fit(dfTrain3)
 print(cluster_model_Agglo.n_clusters)
 print(cluster_model_Agglo.labels_)
-#let_me_see = cluster_model_Agglo.labels_
+let_me_see = cluster_model_Agglo.labels_
 
 ### Training split into training and validation sets ###
 from sklearn.model_selection import KFold
@@ -159,7 +165,71 @@ for train_index, test_index in kf.split(kf_x):
     print('train_y: ', Y_train, 'train_y: ', Y_train)
 
 from sklearn import tree
-from sklearn import f1_score
+from sklearn.metrics import f1_score
+#kf = kfold
+#y = class_labels
+#X = dfTrain3
+min_samples_splits = np.linspace(0.1, 1.0, 10,endpoint=True)
+print(min_samples_splits)
+dfClass_labels = pd.DataFrame(dfTrain2['CLASS_LABEL'])
+
+avg_f1_test = []
+avg_f1_train = []
+avg_n_leaves = []
+
+for mss in min_samples_splits:
+    f1_test = []
+    f1_train = []
+    n_leaves = []
+    
+    for train_index, test_index in kf.split(dfTrain3):
+        X_train, X_test = dfTrain3.iloc[train_index], dfTrain3.iloc[test_index]
+        Y_train, Y_test = dfClass_labels.iloc[train_index], dfClass_labels.iloc[test_index]
+        
+        clf_mss = tree.DecisionTreeClassifier(min_samples_split=mss)
+        clf_mss = clf.fit(X_train, Y_train)
+        
+        Y_test_predicted = clf_mss.predict(X_test)
+        Y_train_predicted = clf_mss.predict(X_train)
+        
+        f1_test.append(f1_score(Y_test,Y_test_predicted,pos_label=0))
+        f1_test.append(f1_score(Y_test,Y_test_predicted,pos_label=0))
+        
+        n_leaves.append(clf.get_n_leaves())
+        
+    avg_f1_test.append(np.mean(f1_test))
+    avg_f1_train.append(np.mean(f1_train))
+    avg_n_leaves.append(np.mean(n_leaves))
+    
+plt.figure(figsize=(4,4))
+plt.plot(min_samples_splits,avg_f1_test,label='testing set')
+plt.plot(min_samples_splits,avg_f1_train, label='training set')
+plt.legend()
+plt.xticks(min_samples_splits)
+plt.grid(color='b',axis='x',linestyle='-.',linewidth=1,alpha=0.2)
+plt.slabel('minimum sample split fraction')
+plt.ylabel('f1')
+
+plt.figure(figsize=(4,4))
+plt.plt(min_samples_splits, avg_n_leaves, label='number of leaf nodes in "X"')
+plt.legend()
+plt.xticks(min_samples_splits)
+plt.yticks(range(2,25,2))
+plt.grid(color='blue',axis='x',linestyle='-.', linewidth=1, alpha=0.2)
+plt.xlabel('minimum sample split fraction')
+plt.ylabel('number')
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
